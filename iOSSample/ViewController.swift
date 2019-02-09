10b8fb98c8c66ffc14aaa6be0f615a82c5adf692
川内翔一朗 <>
2019-02-09 14:14:11 +0900

    nav and tab create

--- iOSSample/ViewController.swift
+++ iOSSample/ViewController.swift
@@ -2,20 +2,29 @@
 //  ViewController.swift
 //  iOSSample
 //
-//  Created by 川内翔一朗 on 2019/02/08.
+//  Created by 川内翔一朗 on 2019/02/09.
 //  Copyright © 2019 SampleApp. All rights reserved.
 //
 
 import UIKit
-import SDWebImage
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
